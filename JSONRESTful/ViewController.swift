0a31afed6197afdfdffbeeb5c8b09adbc2ad48a5
Javier Flores Càrdenas <javier.flores@tecsup.edu.pe>
2021-06-10 15:37:33 -0700

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Javier Flores Càrdenas on 6/10/21.
+//  Copyright © 2021 Javier Flores Càrdenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
